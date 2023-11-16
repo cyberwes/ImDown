b3e9a0195f23dbbb9144dba534e656f361ac6c30
cyberwes <125236619+cyberwes@users.noreply.github.com>
2023-11-17 07:14:52 +1100

    Fixing the I'm Down button & a new logo.  Started working on NewExperience addition

--- ImDown/ImDown/HomeScreen.swift
+++ ImDown/ImDown/HomeScreen.swift
@@ -16,86 +16,108 @@ struct HomeScreen: View {
     var User: Downer
     
     var body: some View {
-        ZStack{
-            User.currentExperience.image.resizable().scaledToFill()
-            VStack{
-                HStack {
-                    Text(User.currentExperience.eventName.uppercased())
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
+        NavigationView {
+            if (User.currentExperience.eventName != "") {
+                ZStack{
+                    User.currentExperience.image.resizable().scaledToFill()
+                    VStack{
+                        HStack {
+                            Text(User.currentExperience.eventName.uppercased())
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            Spacer()
+                            
+                            NavigationLink(destination: NewExperience(User: User)) {
+                                Image(systemName: "square.and.pencil")
+                                    .foregroundColor(Color.black)
+                                    .frame(width: 40, height: 40)
+                                    .background(Color.white)
+                                    .cornerRadius(20)
+                                    .shadow(radius: 10)
+                            }
+                        }
+                        .frame(width: width)
+                        HStack {
+                            Image(systemName: "calendar")
+                                .foregroundColor(Color.white)
+                            Text(User.currentExperience.date)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.white)
+                            Image(systemName: "location")
+                                .foregroundColor(Color.white)
+                            Text(User.currentExperience.location)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.white)
+                            Spacer()
+                        }
+                        .frame(width: width)
+                        .shadow(radius: 4)
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Text("Hosted by " + User.currentExperience.host)
+                                .font(.headline)
+                                .foregroundColor(TextColor)
+                                .padding(12)
+                                .background(ButtonColor)
+                                .cornerRadius(30)
+                        }
+                        .frame(width: width)
                         .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    
-                    Button(action: {}, label: {
-                        VStack{
+                        VStack {
+                            HStack {
+                                Text(User.currentExperience.description)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(TextColor)
+                                    .padding(.vertical, 4.0);
+                            }
+                            .shadow(radius: 4.0)
+                            Button(action: {
+                                User.attend()
+                            }, label: {
+                                Text("I⚡️m Down!")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(TextColor)
+                            })
+                            .frame(width: width, height: 50)
+                            .background(ButtonColor)
+                            .cornerRadius(25.0)
+                            .shadow(radius: 10)
+                            .padding(1.0)
+                        }
+                        .padding()
+                        .frame(width:width)
+                        .cornerRadius(15.0)
+                    }
+                    .padding()
+                }
+                .padding(.horizontal)
+            }
+            else {
+                VStack {
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: NewExperience(User: User)) {
                             Image(systemName: "square.and.pencil")
                                 .foregroundColor(Color.black)
                                 .frame(width: 40, height: 40)
-                            ;
-                        }
-                    } )
-                    .background(Color.white)
-                    .cornerRadius(20)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                }
-                .frame(width: width)
-                HStack {
-                    Image(systemName: "calendar")
-                        .foregroundColor(Color.white)
-                    Text(User.currentExperience.date)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
-                    Image(systemName: "location")
-                        .foregroundColor(Color.white)
-                    Text(User.currentExperience.location)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
-                    Spacer()
-                }
-                .frame(width: width)
-                .shadow(radius: 4)
-                Spacer()
-                HStack {
-                    Spacer()
-                    Text("Hosted by " + User.currentExperience.host)
-                        .font(.headline)
-                        .foregroundColor(TextColor)
-                        .padding(12)
-                        .background(ButtonColor)
-                        .cornerRadius(30)
-                }
-                .frame(width: width)
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                VStack {
-                    HStack {
-                        Text(User.currentExperience.description)
-                            .fontWeight(.semibold)
-                            .foregroundColor(TextColor)
-                            .padding(.vertical, 4.0);
+                                .background(Color.white)
+                                .cornerRadius(20)
+                                .shadow(radius: 10)
+                        }.padding()
                     }
-                    .shadow(radius: 4.0)
-                    Button(action: {
-                        User.attend()
-                    }, label: {
-                        Text("I⚡️m Down!")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .foregroundColor(TextColor)
-                    })
-                    .frame(width: width, height: 50)
-                    .background(ButtonColor)
-                    .cornerRadius(25.0)
-                    .shadow(radius: 10)
-                    .padding(1.0)
+                    Spacer()
+                    Text("Have Fun! 🎉").font(.largeTitle).fontWeight(.bold).foregroundColor(.white)
+                    Text("That experience has been added to your schedule.").foregroundColor(.white)
+                    Spacer()
                 }
+                .background(Color.black)
                 .padding()
-                .frame(width:width)
-                .cornerRadius(15.0)
             }
-            .padding()
         }
-        .padding(.horizontal)
     }
 }
         
