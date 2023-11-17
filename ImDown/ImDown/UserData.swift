ddf109925e4a00dcfa3983ec242e55a5be531cd5
cyberwes <125236619+cyberwes@users.noreply.github.com>
2023-11-17 14:15:19 +1100

    Adding user data dictionaries

---
+++ ImDown/ImDown/UserData.swift
@@ -0,0 +1,25 @@
+//
+//  UserData.swift
+//  ImDown
+//
+//  Created by Wesley Hahn on 17/11/2023.
+//
+
+import Foundation
+
+var Users = [
+    "wesleyhahn" : [
+        "firstName" : "wesley",
+        "lastName" : "hahn",
+        "age" : "28",
+        "pronouns" : "he/him",
+        "location" : "Rosebery, 2018",
+        "about" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+    ],
+    
+    "rahul" : [
+        
+    
+    ]
+]
+
