06b714e8ab7db1c9e1bbfe9d4bfcd5d0db2edff0
Tanvi617 <tanvishah617@gmail.com>
2023-11-17 14:07:49 +1100

    Pushing the world.
    Not required

---
+++ ImDown/ImDown/MysterycardLayout.swift
@@ -0,0 +1,34 @@
+//
+//  MysterycardLayout.swift
+//  ImDown
+//
+//  Created by Tanvi Shah on 17/11/2023.
+//
+
+import SwiftUI
+
+struct MysterycardLayout: View {
+    var body: some View {
+        
+        ZStack {
+//            Rectangle().frame(width:240, height:320)
+            RoundedRectangle(cornerRadius: 10).frame(width:240, height:320).foregroundStyle(Color("primary"))
+            VStack {
+                HStack
+                {
+                    Spacer()
+                    Text ("1.7km away").foregroundStyle(.white)
+                }.padding(20)
+                    Image("MysteryIcon")
+                Text("Hint:").foregroundStyle(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text("You can eat what you make").foregroundStyle(.white)
+                Spacer()
+            }.frame(width:240, height:320)
+        }
+    }
+}
+
+#Preview {
+    MysterycardLayout()
+}
