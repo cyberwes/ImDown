a771df137a8458eb9f11b9d67db1a3b8046e6594
laurenhtong <laurentangy@gmail.com>
2023-11-22 10:11:57 +1100

    wes sucks

--- ImDown/ImDown/TemporaryBackend/UserData.swift
+++ ImDown/ImDown/TemporaryBackend/UserData.swift
@@ -16,7 +16,8 @@ var Users = [
         "age" : "28",
         "pronouns" : "he/him",
         "location" : "Rosebery, 2018",
-        "about" : "Wesley, an avid photographer in his free time, finds joy in capturing moments that tell compelling stories. His passion for travel fuels his exploration of diverse cultures. As a dedicated software engineer, Wesley channels his analytical skills into creating innovative solutions for complex problems."
+        "about" : "Wesley, an avid photographer in his free time, finds joy in capturing moments that tell compelling stories. His passion for travel fuels his exploration of diverse cultures. As a dedicated software engineer, Wesley channels his analytical skills into creating innovative solutions for complex problems.",
+        "interests" : "Photography, travelling, food blogging, coding"
     ],
     "john_doe": [
         "email": "johndoe@gmail.com",
@@ -26,7 +27,9 @@ var Users = [
         "age": "25",
         "pronouns": "he/him",
         "location": "Cityville, 1234",
-        "about": "John is a passionate software developer with a love for hiking and photography. He enjoys experimenting with new technologies and contributing to open-source projects in his free time."
+        "about": "John is a passionate software developer with a love for hiking and photography. He enjoys experimenting with new technologies and contributing to open-source projects in his free time.",
+        "interests" : "Hiking, photography, kayaking, coding"
+
     ],
     "emily_smith": [
         "email": "emilysmith@gmail.com",
@@ -36,7 +39,9 @@ var Users = [
         "age": "22",
         "pronouns": "she/her",
         "location": "Gardentown, 5678",
-        "about": "Emily is a student studying graphic design. She loves creating beautiful and meaningful designs that tell a story. In her free time, she enjoys reading novels and exploring new art galleries."
+        "about": "Emily is a student studying graphic design. She loves creating beautiful and meaningful designs that tell a story. In her free time, she enjoys reading novels and exploring new art galleries.",
+        "interests" : "Graphic design, reading, art, photoshop"
+
     ],
     "alex_turner": [
         "email": "alexturner@gmail.com",
@@ -46,7 +51,9 @@ var Users = [
         "age": "30",
         "pronouns": "they/them",
         "location": "Harbor City, 9876",
-        "about": "Alex is a nature enthusiast and a wildlife photographer. They love capturing the beauty of the outdoors and sharing it with the world."
+        "about": "Alex is a nature enthusiast and a wildlife photographer. They love capturing the beauty of the outdoors and sharing it with the world.",
+        "interests" : "Hiking, photography, blogging, rock climbing"
+
     ],
     "olivia_jones": [
         "email": "oliviajones@gmail.com",
@@ -56,7 +63,9 @@ var Users = [
         "age": "27",
         "pronouns": "she/her",
         "location": "Meadowville, 5432",
-        "about": "Olivia is a fitness trainer and nutritionist. She is dedicated to helping others achieve their health and wellness goals through personalized training programs and dietary guidance."
+        "about": "Olivia is a fitness trainer and nutritionist. She is dedicated to helping others achieve their health and wellness goals through personalized training programs and dietary guidance.",
+        "interests" : "Cardio, yoga, cooking, baking"
+
     ],
     "michael_brown": [
         "email": "michaelbrown@gmail.com",
@@ -66,7 +75,9 @@ var Users = [
         "age": "35",
         "pronouns": "he/him",
         "location": "Riverside, 2468",
-        "about": "Michael is a travel blogger and adventure seeker. He loves exploring new cultures, trying exotic foods, and sharing his experiences with his followers."
+        "about": "Michael is a travel blogger and adventure seeker. He loves exploring new cultures, trying exotic foods, and sharing his experiences with his followers.",
+        "interests" : "Travelling, blogging, photography, mountain biking"
+
     ],
 ]
 
